fd075e1d8d63c69ead81b3a94d39309cb706c48d
mad-ev <alessio.mart87@gmail.com>
2026-03-29 14:56:43 +0200

    feat: added softDeletes trait to Workshop model

--- app/Models/Workshop.php
+++ app/Models/Workshop.php
@@ -6,13 +6,14 @@ use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 #[Fillable(['title', 'desciption', 'scheduled_at', 'capacity'])]
 
 class Workshop extends Model
 {
-    /** @use HasFactory<\Database\Factories\WorkshopFactory> */
-    use HasFactory;
+	/** @use HasFactory<\Database\Factories\WorkshopFactory> */
+	use HasFactory, SoftDeletes;
 
 	protected $cast = [
 		'scheduled_at' => 'datetime',
@@ -21,6 +22,6 @@ class Workshop extends Model
 	public function users(): BelongsToMany
 	{
 		return $this->belongsToMany(User::class)
-		->withTimestamps();
+			->withTimestamps();
 	}
 }
